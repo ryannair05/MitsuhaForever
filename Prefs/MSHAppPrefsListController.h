c45d3b3c8e335f399cbb7b732e4b25010d83ca60
ConorTheDev <con0r.b@protonmail.com>
2019-12-06 19:33:00 +0000

    (Prefs) Reformat code

--- Prefs/MSHAppPrefsListController.h
+++ Prefs/MSHAppPrefsListController.h
@@ -1,7 +1,7 @@
+#import <CepheiPrefs/HBAppearanceSettings.h>
+#import <CepheiPrefs/HBListController.h>
 #import <Preferences/PSListController.h>
 #import <Preferences/PSSpecifier.h>
-#import <CepheiPrefs/HBListController.h>
-#import <CepheiPrefs/HBAppearanceSettings.h>
 
 @interface MSHAppPrefsListController : HBListController
 
--- Prefs/MSHPrefsListController.h
+++ Prefs/MSHPrefsListController.h
@@ -1,13 +1,13 @@
+#import "../MSHUtils.h"
+#import <Cephei/HBPreferences.h>
+#import <CepheiPrefs/HBAppearanceSettings.h>
+#import <CepheiPrefs/HBRootListController.h>
 #import <Preferences/PSListController.h>
 #import <Preferences/PSSpecifier.h>
-#import <CepheiPrefs/HBRootListController.h>
-#import <CepheiPrefs/HBAppearanceSettings.h>
-#import <Cephei/HBPreferences.h>
 #import <spawn.h>
-#import "../MSHUtils.h"
 
 @interface MSHPrefsListController : HBListController
-    - (void)resetPrefs:(id)sender;
-    - (void)respring:(id)sender;
-    - (void)restartmsd:(id)sender;
+- (void)resetPrefs:(id)sender;
+- (void)respring:(id)sender;
+- (void)restartmsd:(id)sender;
 @end
