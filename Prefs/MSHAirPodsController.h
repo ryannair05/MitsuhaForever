78b58db3198f9f5f455367f30ea0d1d1e091a2b3
ConorTheDev <con0r.b@protonmail.com>
2019-12-08 15:05:02 +0000

    (Prefs) Add sensitivity boost option for AirPods 2

---
+++ Prefs/MSHAirPodsController.h
@@ -0,0 +1,11 @@
+#import "../MSHUtils.h"
+#import <Cephei/HBPreferences.h>
+#import <CepheiPrefs/HBAppearanceSettings.h>
+#import <CepheiPrefs/HBRootListController.h>
+#import <Preferences/PSListController.h>
+#import <Preferences/PSSpecifier.h>
+#import <spawn.h>
+
+@interface MSHAirPodsController : HBListController
+
+@end
---
+++ Prefs/MSHAirPodsController.m
@@ -0,0 +1,46 @@
+#import "MSHAirPodsController.h"
+#import "MSHPrefsListController.h"
+
+@implementation MSHAirPodsController
+- (instancetype)init {
+  self = [super init];
+
+  if (self) {
+    HBAppearanceSettings *appearanceSettings =
+        [[HBAppearanceSettings alloc] init];
+    appearanceSettings.tintColor = [UIColor colorWithRed:238.0f / 255.0f
+                                                   green:100.0f / 255.0f
+                                                    blue:92.0f / 255.0f
+                                                   alpha:1];
+    appearanceSettings.tableViewCellSeparatorColor = [UIColor colorWithWhite:0
+                                                                       alpha:0];
+    self.hb_appearanceSettings = appearanceSettings;
+  }
+
+  return self;
+}
+
+- (id)specifiers {
+  if (_specifiers == nil) {
+    _specifiers = [[self loadSpecifiersFromPlistName:@"AirPods"
+                                              target:self] retain];
+  }
+  return _specifiers;
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+  [super viewWillAppear:animated];
+
+  CGRect frame = self.table.bounds;
+  frame.origin.y = -frame.size.height;
+
+  [self.navigationController.navigationController.navigationBar
+      setShadowImage:[UIImage new]];
+  self.navigationController.navigationController.navigationBar.translucent =
+      YES;
+}
+
+- (bool)shouldReloadSpecifiersOnResume {
+  return false;
+}
+@end
