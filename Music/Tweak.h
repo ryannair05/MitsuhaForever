9a38cae1a39d98e81f0fb5f52aa75c2ab8c46d33
ConorTheDev <con0r.b@protonmail.com>
2019-12-15 10:23:58 +0000

    (Music) Format Tweah.h

--- Music/Tweak.h
+++ Music/Tweak.h
@@ -6,15 +6,18 @@
 //  Copyright © 2017 c0ldra1n. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
 #import <Mitsuha/MSHView.h>
 #import <MitsuhaInfinity/MSHConfig.h>
+#import <UIKit/UIKit.h>
 
 @interface MusicArtworkComponentImageView : UIImageView
--(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context;
--(void)readjustWaveColor;
+- (void)observeValueForKeyPath:(NSString *)keyPath
+                      ofObject:(id)object
+                        change:(NSDictionary *)change
+                       context:(void *)context;
+- (void)readjustWaveColor;
 @end
 
 @interface MusicNowPlayingControlsViewController : UIViewController
-@property (retain,nonatomic) MSHView *mshView;
+@property(retain, nonatomic) MSHView *mshView;
 @end
