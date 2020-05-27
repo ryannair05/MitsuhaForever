19b5554768ff481323bbd73dd9dea12ed8de5a6f
Conor Byrne <con0r.b@protonmail.com>
2020-05-27 09:57:35 +0100

    Fix custom colours + version bump - Closes #2

--- Prefs/MSHFAppPrefsListController.h
+++ Prefs/MSHFAppPrefsListController.h
@@ -2,6 +2,7 @@
 #import <Preferences/PSSpecifier.h>
 #import <CepheiPrefs/HBListController.h>
 #import <CepheiPrefs/HBAppearanceSettings.h>
+#import <libcolorpicker.h>
 
 @interface MSHFAppPrefsListController : HBListController
 
